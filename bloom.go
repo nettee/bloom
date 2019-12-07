d31fc5ce0241c94814620e4497729ec1f7123407
William <nettee.liu@gmail.com>
2019-12-07 16:02:41 +0800

    set cli basic

--- bloom.go
+++ bloom.go
@@ -1,10 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"github.com/urfave/cli/v2"
+	"log"
 	"os"
 )
 
 func main() {
-	(&cli.App{}).Run(os.Args)
+
+	app := &cli.App {
+		Name: "bloom",
+		Version: "0.0.1",
+		Authors: [] *cli.Author {
+			{
+				Name:  "nettee",
+				Email: "nettee.liu@gmail.com",
+			},
+		},
+		Usage: "Markdown article manager",
+		Commands: [] *cli.Command{
+			{
+				Name:    "new",
+				Aliases: [] string{"n"},
+				Usage:   "create article or collection",
+				Action: func(c *cli.Context) error {
+					fmt.Println("new")
+					return nil
+				},
+			},
+			{
+				Name:    "list",
+				Aliases: [] string{"l"},
+				Usage:   "list articles and collections",
+				Action: func(c *cli.Context) error {
+					fmt.Println("list")
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
