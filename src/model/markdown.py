ab3fefc0218742b134355bf1d0ee4f8c4e15c510
nettee <nettee.liu@gmail.com>
2020-01-07 14:47:15 +0800

    rewriting markdown parser

--- src/model/markdown.py
+++ src/model/markdown.py
@@ -1,7 +1,139 @@
+import re
 from abc import ABCMeta, abstractmethod
+from dataclasses import dataclass
+from typing import List
 
 
+@dataclass
+class Line:
+    text: str
+
+    def is_empty(self) -> bool:
+        return len(self.text) == 0
+
+    def is_heading(self) -> bool:
+        return self.text.startswith('#')
+
+    def is_image(self) -> bool:
+        return self.text.startswith('!')
+
+    def is_quoted(self) -> bool:
+        return self.text.startswith('>')
+
+    def is_math_block_border(self) -> bool:
+        return self.text.startswith('$$')
+
+    def is_code_block_border(self) -> bool:
+        return self.text.startswith('```')
+
+
+class Paragraph(metaclass=ABCMeta):
+
+    @abstractmethod
+    def line_strings(self) -> List[str]:
+        pass
+
+    def string(self) -> str:
+        return '\n'.join(self.line_strings())
+
+    def __str__(self):
+        return self.string()
+
+
+@dataclass
+class Heading(Paragraph):
+    level: int
+    text: str
+
+    def line_strings(self) -> List[str]:
+        return [self.__str__()]
+
+    def __str__(self):
+        return '#' * self.level + ' ' + self.text
+
+
+@dataclass
+class NormalParagraph(Paragraph):
+    lines: List[str]
+
+    def line_strings(self) -> List[str]:
+        return self.lines
+
+
+@dataclass
+class Image(Paragraph):
+    caption: str
+    uri: str
+
+    def line_strings(self) -> List[str]:
+        return [self.__str__()]
+
+    def __str__(self):
+        return f'![{self.caption}]({self.uri})'
+
+    def is_local(self) -> bool:
+        return not self.is_online()
+
+    def is_online(self) -> bool:
+        return self.uri.startswith('http://') \
+               or self.uri.startswith('https://')
+
+
+@dataclass
+class Quote(Paragraph):
+    paragraphs: List[Paragraph]
+
+    def line_strings(self) -> List[str]:
+        # TODO
+        raise NotImplementedError()
+
+
+@dataclass
+class CodeBlock(Paragraph):
+    language: str
+    lines: List[str]
+
+    def line_strings(self) -> List[str]:
+        res = ['```' + self.language]
+        res.extend(self.lines)
+        res.append('```')
+        return res
+
+
+@dataclass
+class MathBlock(Paragraph):
+    lines: List[str]
+
+    def line_strings(self) -> List[str]:
+        res = ['$$']
+        res.extend(self.lines)
+        res.append('$$')
+        return res
+
+
+@dataclass
+class HexoHeader(Paragraph):
+    # TODO
+    title: str
+
+    def line_strings(self) -> List[str]:
+        # TODO
+        raise NotImplementedError()
+
+
+class ReadMore(Paragraph):
+
+    def line_strings(self) -> List[str]:
+        return [self.__str__()]
+
+    def __str__(self):
+        return '<!-- more -->'
+
+
+@dataclass
 class MarkdownDoc:
+    title: str
+    body: List[Paragraph]
 
     @staticmethod
     def from_file(filename):
@@ -17,12 +149,18 @@ class MarkdownDoc:
     @staticmethod
     def from_lines(lines):
         parser = MarkdownParser.from_lines(lines)
-        print(parser)
-        # TODO parse it
+        return parser.parse()
 
-    def __init__(self, title, body):
-        self.title = title
-        self.body = body
+    # For debug only
+    def _show(self) -> None:
+        print('title:', self.title)
+        for paragraph in self.body:
+            print(repr(paragraph))
+            print()
+
+
+class MarkdownParseException(Exception):
+    pass
 
 
 class MarkdownParser:
@@ -39,6 +177,112 @@ class MarkdownParser:
         self.lines = lines
         self.pos = 0
 
+    def parse(self) -> MarkdownDoc:
+        paragraphs = self.parse_paragraphs()
+        return MarkdownDoc(title='', body=paragraphs)
+
+    def parse_paragraphs(self) -> List[Paragraph]:
+        paragraphs = []
+        while not self.eof():
+            line = self.next_line()
+            if line.is_empty():
+                self.consume_line()
+            else:
+                paragraphs.append(self.parse_paragraph())
+        return paragraphs
+
+    def parse_paragraph(self) -> Paragraph:
+        line = self.next_line()
+        if line.is_heading():
+            return self.parse_heading()
+        elif line.is_image():
+            return self.parse_image()
+        elif line.is_quoted():
+            return self.parse_quote()
+        elif line.is_code_block_border():
+            return self.parse_code_block()
+        elif line.is_math_block_border():
+            return self.parse_math_block()
+        else:
+            return self.parse_normal_paragraph()
+
+    def parse_heading(self) -> Heading:
+        line = self.consume_line()
+        return self.parse_heading_from_line(line.text)
+
+    @staticmethod
+    def parse_heading_from_line(line: str) -> Heading:
+        # TODO restrict the number of #'s
+        pattern = re.compile(r'^(#+)\s+(.+)$')
+        m = re.match(pattern, line)
+        if m is None:
+            raise MarkdownParseException(f"Invalid heading `{line}'")
+        level = len(m.group(1))
+        text = m.group(2)
+        return Heading(level=level, text=text)
+
+    def parse_image(self) -> Image:
+        line = self.consume_line()
+        return self.parse_image_from_line(line.text)
+
+    @staticmethod
+    def parse_image_from_line(line: str) -> Image:
+        pattern = re.compile(r'!\[(.*)\]\((.*)\)')
+        m = re.match(pattern, line)
+        if m is None:
+            raise MarkdownParseException(f"Invalid image `{line}'")
+
+        caption = m.group(1)
+        uri = m.group(2)
+        return Image(caption=caption, uri=uri)
+
+    def parse_quote(self) -> Quote:
+        lines = self.consume_while(lambda line: line.is_quoted())
+        return Quote([])
+
+    def parse_code_block(self) -> CodeBlock:
+        start_line = self.consume_line()
+        language = self.parse_code_language_from_line(start_line.text)
+        lines = self.consume_while(lambda line: not line.is_code_block_border())
+        self.consume_line()
+        return CodeBlock(language=language, lines=[l.text for l in lines])
+
+    @staticmethod
+    def parse_code_language_from_line(line: str) -> str:
+        pattern = re.compile(r'```(\S*)')
+        m = re.match(pattern, line)
+        if m is None:
+            raise MarkdownParseException(f"Invalid code block start `{line}'")
+
+        language = m.group(1)
+        return language
+
+    def parse_math_block(self) -> MathBlock:
+        self.consume_line()
+        lines = self.consume_while(lambda line: not line.is_math_block_border())
+        self.consume_line()
+        return MathBlock([l.text for l in lines])
+
+    def parse_normal_paragraph(self) -> NormalParagraph:
+        lines = self.consume_while(lambda line: not line.is_empty())
+        return NormalParagraph([l.text for l in lines])
+
+    def consume_while(self, predicate) -> List[Line]:
+        lines = []
+        while not self.eof() and predicate(self.next_line()):
+            lines.append(self.consume_line())
+        return lines
+
+    def consume_line(self) -> Line:
+        line = Line(self.lines[self.pos])
+        self.pos += 1
+        return line
+
+    def next_line(self) -> Line:
+        return Line(self.lines[self.pos])
+
+    def eof(self) -> bool:
+        return self.pos >= len(self.lines)
 
 # ==============================
 
--- test/model/test_markdown.py
+++ test/model/test_markdown.py
@@ -1,15 +1,27 @@
+import logging
+
 import pytest
 
-from model.markdown import MarkdownDoc
+from model.markdown import MarkdownDoc, MarkdownParser
+
+logger = logging.getLogger(__name__)
 
 doc_path = '/Users/william/bloomstore/LeetCode 例题精讲/03-从二叉树遍历到回溯算法/03-从二叉树遍历到回溯算法.md'
 
 
-class TestMarkdown:
+def test_from_file():
+    doc = MarkdownDoc.from_file(doc_path)
+    doc._show()
 
-    def test_from_file(self):
-        MarkdownDoc.from_file(doc_path)
+
+def test_parse_heading():
+    line = '###     Some Text'
+    heading = MarkdownParser.parse_heading_from_line(line)
+    assert heading.level == 3
+    assert heading.text == 'Some Text'
+    print()
+    print(repr(heading))
 
 
 if __name__ == '__main__':
-    pytest.main(['-q', 'test_markdown.py'])
+    pytest.main(['-s', 'test_markdown.py'])
