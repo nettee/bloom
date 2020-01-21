5f556aff1fd8ce36904df524cbd3522220815b44
nettee <nettee.liu@gmail.com>
2020-01-21 18:15:59 +0800

    remove old files and reconstruct package

--- bloom/__init__.py
+++ bloom/__init__.py
@@ -1,12 +1,17 @@
+from pathlib import Path
+
 import fire
+
+from bloom.core.publish import publish
+from bloom.model.article import Article
 
 
 class Bloom:
     """Blog output manager"""
 
-    def publish(self, article_path):
-        print('bloom publish')
-        print(article_path)
+    def publish(self, article_path: Path = Path('.')):
+        article = Article.open(article_path)
+        publish(article)
 
 
 def main():
--- src/post/listing.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from base import config
-
-
-def list_all():
-    root = Path(config.bloomstore)
-    for item in root.iterdir():
-        print(item)
